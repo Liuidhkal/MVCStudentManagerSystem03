41456d7fdddc21589eb234e694caec50726d305c
lfz <1915565819@qq.com>
2023-05-05 11:05:24 +0800

    proxy port commit

--- src/com/mvc/controller/Test.java
+++ src/com/mvc/controller/Test.java
@@ -3,5 +3,6 @@ package com.mvc.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("修改完proxy端口后测试");
     }
 }
